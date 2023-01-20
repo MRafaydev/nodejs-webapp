d29beca7da764720ed7a48540c013847063adf40
Muhammad Rafay <muhammadrafay7655@gmail.com>
2023-01-20 16:57:33 +0500

    Initialized Project

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:lts-alpine3.17
+WORKDIR /app
+COPY . .
+RUN npm install
+EXPOSE 3000
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hello World!!!!</h2>");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h2>This is Login Page!!</h2>");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
